00dbfd565d7dd7e330a08b03ed983e2608a8e0d9
R. Bassett Jr. (Tatwi) <rbassettjr@tpot.ca>
2018-08-12 01:39:34 -0400

    Fixed sound not playing in Google Chrome version 55 or higher...

--- src/boot.js
+++ src/boot.js
@@ -24,5 +24,41 @@ RocketTux.Boot.prototype = {
 	this.game.physics.startSystem(Phaser.Physics.ARCADE);
     
     this.state.start('Preload');
+    
+    // Hack I copy/pasted from the internet to make sound work again...
+    // Author https://github.com/AleBles
+    if (this.game.device.android && this.game.device.chrome && this.game.device.chromeVersion >= 55) {
+        this.game.sound.touchLocked = true;
+        this.game.input.touch.addTouchLockCallback(function () {
+            if (this.noAudio || !this.touchLocked || this._unlockSource !== null) {
+                return true;
+            }
+            if (this.usingWebAudio) {
+                // Create empty buffer and play it
+                // The SoundManager.update loop captures the state of it and then resets touchLocked to false
+
+                var buffer = this.context.createBuffer(1, 1, 22050);
+                this._unlockSource = this.context.createBufferSource();
+                this._unlockSource.buffer = buffer;
+                this._unlockSource.connect(this.context.destination);
+
+                if (this._unlockSource.start === undefined) {
+                    this._unlockSource.noteOn(0);
+                }
+                else {
+                    this._unlockSource.start(0);
+                }
+
+                //Hello Chrome 55!
+                if (this._unlockSource.context.state === 'suspended') {
+                    this._unlockSource.context.resume();
+                }
+            }
+
+            //  We can remove the event because we've done what we needed (started the unlock sound playing)
+            return true;
+
+        }, this.game.sound, true);
+    }
   }
 };
