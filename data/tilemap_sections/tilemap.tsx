1a9912c7ee5e9404757681e953df4bb871a4bdc1
R. Bassett Jr <alsopeachy@gmail.com>
2018-11-09 15:32:08 -0500

    QA pass on remaining existing map sections. Also enabled several new map sections.

--- data/tilemap_sections/tilemap.tsx
+++ data/tilemap_sections/tilemap.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="world" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+<tileset version="1.2" tiledversion="1.2.0" name="world" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
  <image source="../world.png" width="2048" height="2048"/>
 </tileset>
