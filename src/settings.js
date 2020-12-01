d70ac9879cf9d3b6014781a7e110fe1d77495ca1
R. Bassett Jr. (Tatwi) <alsopeachy@gmail.com>
2020-12-01 05:37:33 -0500

    Settings: Can now select chassis color of game device in menus

--- src/settings.js
+++ src/settings.js
@@ -7,6 +7,17 @@ RocketTux.Settings.prototype = {
 		// console.log("Settings state loaded");
 	},
 	preload: function() {
+		
+	},
+	create: function() {
+	  // Background image
+		this.bgImage = this.game.add.sprite(0, 0, 'skies-special');
+		this.bgImage.animations.add('stand', [3], 1, true);
+		this.bgImage.scale.setTo(1.25, 0.71); //wide, tall
+		this.bgImage.width = this.game.width;
+		this.bgImage.play('stand');
+		this.bgImage.fixedToCamera = true;
+		
 		this.style = { 
 			font: "11px Verdana", 
 			fill: "#ffffff", align: "left"
@@ -20,9 +31,33 @@ RocketTux.Settings.prototype = {
 		};
 		this.titleText = this.game.add.text(0, 0, "Settings", this.titleStyle);
 		this.titleText.setTextBounds(0, 0, this.game.width, 30);
-	},
-	create: function() {
-	  
+		
+		this.headingsStyle = { 
+			font: "22px Verdana", 
+			fill: "#ffffff", align: "left"
+		};
+		this.headingsLeft = this.game.add.text(16, 64, "", this.headingsStyle);
+		this.headingsLeft.text = "Game Device Color\n\n\n\n";
+		//this.headingsLeft.text += "Disable Music\n\n\n\n";
+		this.headingsRight = this.game.add.text(this.game.width/2+16, 64, "", this.headingsStyle);
+		this.headingsRight.text = "Screen Font Color\n\n\n\n";
+		//this.headingsRight.text += "Controller Style \n\n\n\n";
+		
+		// Game device color buttons
+		var gdColors = ['0xB7B7B7','0x333333','0xB5000E','0x1EDC00','0x0042A9','0xFFFF84','0x0AB9E6','0x69E696','0xFF3278','0x4655E6','0xB400F5','0xFFFF00'];
+		this.btGDC = this.game.add.group();
+		var gdcBtn;
+		for (i = 0; i < 12; i++){
+			gdcBtn = this.game.add.button(16 + i*40, 96, 'ui-map', this.setGDColor, this, 'check-over', 'check-out', 'check-down');
+			gdcBtn.tint = gdColors[i];
+			this.btGDC.add(gdcBtn);
+			
+			// Set current as selected
+			if (gdColors[i] === RocketTux.mainMenuColor){
+				gdcBtn.setFrames('check-selected', 'check-selected', 'check-selected');
+			}
+		}
+		
 	}, 
 	update: function() {
 		if (this.game.input.keyboard.downDuration(Phaser.Keyboard.ESC, 1)){
@@ -31,5 +66,18 @@ RocketTux.Settings.prototype = {
 	},
 	shutdown: function(){
 		// console.log("Settings state exited");
+	},
+	setGDColor: function (btn) {
+		// Reset all to unselected 
+		this.btGDC.forEach(function(gdcBtn) {
+			gdcBtn.setFrames('check-over', 'check-out', 'check-down');
+		}, this, true);
+		
+		// Set selected
+		btn.setFrames('check-selected', 'check-selected', 'check-selected');
+		
+		// Update color
+		localStorage.setItem('RocketTux-mainMenuColor', btn.tint);
+		RocketTux.mainMenuColor = btn.tint;
 	}
 };
